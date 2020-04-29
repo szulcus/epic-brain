db4bcc853ffd9661de959c70ffead653ed66154f
jakub104 <jakubzlezena@wp.pl>
2020-04-29 22:59:41 +0200

    Updates

---
+++ precache-manifest.a68cc12f1c79e4d8f61368f62042c9b6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f6cc6a3415d51812de4655e50a638b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06f2a6780b58adc40788",
+    "url": "/static/css/main.fdfde704.chunk.css"
+  },
+  {
+    "revision": "5aa3b7a8059143f3e3ea",
+    "url": "/static/js/2.9d351b71.chunk.js"
+  },
+  {
+    "revision": "ca68ede49512130189af78bfd66ec5db",
+    "url": "/static/js/2.9d351b71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06f2a6780b58adc40788",
+    "url": "/static/js/main.e9503f27.chunk.js"
+  },
+  {
+    "revision": "09090d600e763be1d9f9",
+    "url": "/static/js/runtime-main.bf7ba005.js"
+  },
+  {
+    "revision": "08adfec67aab6e7764b839ce99ac6ab0",
+    "url": "/static/media/logo.08adfec6.png"
+  },
+  {
+    "revision": "68f836b8d99832b1c40dc70aa09a58a1",
+    "url": "/static/media/scroll.68f836b8.svg"
+  },
+  {
+    "revision": "793e6f6d3ed9d9388cd3a2bf51ffe469",
+    "url": "/static/media/vector-logo-line.793e6f6d.svg"
+  },
+  {
+    "revision": "b6a4d60de9a97b1df6a283e48192f567",
+    "url": "/static/media/vector-logo-triangle.b6a4d60d.svg"
+  }
+]);
