8c09b9fd26edb164b820fe312181618e0f9906c5
jakub104 <jakubzlezena@wp.pl>
2020-05-02 20:40:26 +0200

    Updates

---
+++ precache-manifest.f2dabf9cc0421108c51c3a0331c32257.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e800aa54b77de8c1971451ff047557ed",
+    "url": "/epic-brain/index.html"
+  },
+  {
+    "revision": "74a2c530293bb8a80850",
+    "url": "/epic-brain/static/css/main.fdfde704.chunk.css"
+  },
+  {
+    "revision": "2afb1d84ebe2daeac6b7",
+    "url": "/epic-brain/static/js/2.9818498f.chunk.js"
+  },
+  {
+    "revision": "ca68ede49512130189af78bfd66ec5db",
+    "url": "/epic-brain/static/js/2.9818498f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74a2c530293bb8a80850",
+    "url": "/epic-brain/static/js/main.f0e3e796.chunk.js"
+  },
+  {
+    "revision": "6cf36460da8307ad393e",
+    "url": "/epic-brain/static/js/runtime-main.c886a7f2.js"
+  },
+  {
+    "revision": "22d09fa84570515db3e2a3e7e20d7d67",
+    "url": "/epic-brain/static/media/hello.22d09fa8.png"
+  },
+  {
+    "revision": "da9261010de73cffc272d85fdfc8f251",
+    "url": "/epic-brain/static/media/loader.da926101.svg"
+  },
+  {
+    "revision": "08adfec67aab6e7764b839ce99ac6ab0",
+    "url": "/epic-brain/static/media/logo.08adfec6.png"
+  },
+  {
+    "revision": "68f836b8d99832b1c40dc70aa09a58a1",
+    "url": "/epic-brain/static/media/scroll.68f836b8.svg"
+  },
+  {
+    "revision": "793e6f6d3ed9d9388cd3a2bf51ffe469",
+    "url": "/epic-brain/static/media/vector-logo-line.793e6f6d.svg"
+  },
+  {
+    "revision": "b6a4d60de9a97b1df6a283e48192f567",
+    "url": "/epic-brain/static/media/vector-logo-triangle.b6a4d60d.svg"
+  }
+]);
